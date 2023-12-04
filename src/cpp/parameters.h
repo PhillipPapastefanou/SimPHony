536294877872116c8b2240a0773957a3dfba424b
Phillip Papastefanou <papa@tum.de>
2023-12-04 11:37:55 +0100

    Version 1.1: - Mainly added support for arbitary time precision

--- src/cpp/parameters.h
+++ src/cpp/parameters.h
@@ -10,6 +10,15 @@ public:
     Parameters();
     ~Parameters();
 
+    const double SEC_IN_DAY = 86400.0;
+    const double CM_IN_M = 100.0;
+    const double KG_H2O_To_Mol = 1000.0 / 18.0;
+    const double PaToMPa = std::pow(10, -6);
+    const double MPaToPa = std::pow(10, +6);
+    //const double PI = std::numbers::pi;
+    //Todo Use C constants again. Does not work on the MPI cluster atm
+    const double PI = 3.14159265359;
+
     // Root zone depth [m]
     double root_zone_depth = 0.3;
     // Root area index [1]
@@ -31,13 +40,15 @@ public:
      double eta_LS = 1.0; // 1.0 = Water
     // Stem hydraulic capacity [mol m-3 MPa-1]
     // From Meinzer et al. 2011 Figure 13.2
-    // Range: 10 - 500 kg m-3 MPa-1
-    double stem_hydraulic_capacitance_max = 20.0 * 1000 / 18.0;
+    // Range: 10 - 500 kg H2O m-3 MPa-1
+    // Convert to [mol H2O m-3 MPa-1]
+    double stem_hydraulic_capacitance_max = 20.0 * KG_H2O_To_Mol;
 
-    // Xylem saturated Hydraulic conductivity [mol m-1 30min-1 MPa]
-    // Values measured around 10 ~ 250, but could be up 3000
-    double k_xylem_sat = 150;
-    // Huber value [m2 m-2] equals 1/klatosa
+    // Xylem saturated Hydraulic conductivity [mol m-1 s-1 MPa-1]
+    // Meinzer et al 2010: Could be up 1-10 kg H2O m-1 s-1 MPa-1 (Maximum specific conductivity)
+    // Meinzer et al 2010: 0.05- 10 kg H2O m-1 s-1 MPa-1 (Xylem specific conductivity)
+    double k_xylem_sat = 150/1800.0;
+    // Huber value [m2 m-2] equals 1/klatosa (leaf area to sapwood area)
     // From sperry et al
     double huber_value = 1.0/3600.0;
 
@@ -47,8 +58,7 @@ public:
     double d_50_s = 5.0;
 
 
-
-    // Leaf hydraulic conductance [mol m-2 MPa]
+    // Leaf hydraulic conductance [mol m-2 MPa -1]
     // range 0.2 - 1.2 from Blackmann and Brodribb 2011
     double leaf_hydraulic_capacitance = 1.0;
     // Leaf area index [m2 m-2]
@@ -81,15 +91,15 @@ public:
     // Jackson rooting parameter [-]
     double jackson_root_beta  = 0.96;
 
-    // Saturated soil hydraulic conductance [m 30min-1]
-    // Van Looy et al 4.8 - 62 [cm/d]
+    // Saturated soil hydraulic conductance [m s-1]
+    // Van Looy et al 4.8 - 62 [cm d-1]
     double _k_soil_sat_cm_d = 15.0;
-    double k_soil_sat = _k_soil_sat_cm_d / 100.0 / 86400.0 * 1800;
+    double k_soil_sat = _k_soil_sat_cm_d / CM_IN_M / SEC_IN_DAY;
 
     // Saturated volumetric water content [m3 m-3]
     double theta_s = 0.8;
 
-    // Empircial water content multplier [-]
+    // Empirical water content multiplier [-]
     // Should be 1.0 for runs where the given wcont is known precisely
     double theta_emp_multiplier = 1.0;
 
@@ -106,10 +116,7 @@ public:
     double camp_psi_soil_ref = -1.50042 * std::pow(10, -6);
 
 
-    const double PaToMPa = std::pow(10, -6);
-    const double MPaToPa = std::pow(10, +6);
-    //const double PI = std::numbers::pi;
-    const double PI = 3.14159;
+
 
     double solver_precision = 1E-10;
 
