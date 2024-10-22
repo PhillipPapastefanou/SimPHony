c4210778944d1b76cbe7b72ec3a3feea4a781be6
Phillip Papastefanou <papa@tum.de>
2024-10-22 10:32:50 +0200

    Added remaining output files

--- src/core/cpp/io/input_swiss_indiv_variation.cpp
+++ src/core/cpp/io/input_swiss_indiv_variation.cpp
@@ -2,32 +2,35 @@
 // Created by Phillip on 08/10/2024.
 //
 
-#include "input_swiss_std_variation.h"
+#include "input_swiss_indiv_variation.h"
 #include <numeric>
 #include <iostream>
 
-Input_Swiss_Std_Variation::Input_Swiss_Std_Variation(): Input(){
+Input_Swiss_Indiv_Variation::Input_Swiss_Indiv_Variation(): Input(){
 
 }
 
 
 
-void Input_Swiss_Std_Variation::Add_Forcing_File(std::string file) {
+void Input_Swiss_Indiv_Variation::Add_Forcing_File(std::string file) {
     forcing_file = file;
 }
 
-void Input_Swiss_Std_Variation::Add_Soilwater_File(std::string file) {
+void Input_Swiss_Indiv_Variation::Add_Soilwater_File(std::string file) {
     theta_file = file;
 }
 
-void Input_Swiss_Std_Variation::Read_N_Parse() {
+void Input_Swiss_Indiv_Variation::Read_N_Parse() {
 
     theta_parser = std::make_unique<InputCollection>(theta_file, true, ',');
     forcing_parser = std::make_unique<InputCollection>(forcing_file, true, ',');
 
-    vector<string> soil_water_indexes({"v10_adapted","v40_adapted","v80_adapted","sd"});
+    vector<string> soil_water_indexes({"theta_10cm","theta_std_10cm",
+                                       "theta_40cm","theta_std_40cm",
+                                       "theta_80cm","theta_std_80cm"});
+
     std::string format = "%Y-%m-%d %H:%M:%S";
-    theta_parser->init_regular("date", format);
+    theta_parser->init_regular("dates", format);
 
     // Read all soil water inpults
     vector<vector<float> > soil_water_input = theta_parser->get_data(soil_water_indexes);
@@ -58,15 +61,16 @@ void Input_Swiss_Std_Variation::Read_N_Parse() {
         exit(99);
     }
 
-    // Each soil location has three soil layer depths
+    // Each soil location has three soil layer depths and std
     for (int i = 0; i < soil_water_input.size(); ++i) {
         vector<float> sl(3);
+        vector<float> sl_std(3);
         for (int s = 0; s < 3; ++s) {
-            sl[s] = soil_water_input[i][s];
+            sl[s] = soil_water_input[i][2*s];
+            sl_std[s] = soil_water_input[i][2*s + 1];
         }
         theta_per_layer.push_back(sl);
-        // Theta sd is in the four column of the dataset
-        theta_sd.push_back(soil_water_input[i][3]);
+        theta_sd_per_layer.push_back(sl_std);
     }
 
     // Slice forcing input according to indexes as we might have different dates
--- src/core/cpp/io/input_swiss_indiv_variation.h
+++ src/core/cpp/io/input_swiss_indiv_variation.h
@@ -14,10 +14,10 @@ using std::string;
 using std::vector;
 using std::unique_ptr;
 
-class Input_Swiss_Std_Variation: public Input{
+class Input_Swiss_Indiv_Variation: public Input{
 
 public:
-    Input_Swiss_Std_Variation();
+    Input_Swiss_Indiv_Variation();
 
     void Read_N_Parse() override;
     void Add_Forcing_File(std::string file) override;
