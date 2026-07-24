ba92f2ec5dec797fdec7b7819345b6bc9441a8c8
PP <papa@tum.de>
2026-07-24 21:19:35 +0200

    version two complete - removed parameters

--- src/core/cpp/bindings.cpp
+++ src/core/cpp/bindings.cpp
@@ -67,6 +67,7 @@ EMSCRIPTEN_BINDINGS(simphony_web_module) {
         .property("psi88_xylem", &Parameters::psi88_xylem)
         .property("psi_leaf_50_close", &Parameters::psi_leaf_50_close)
         .property("d_50_close", &Parameters::d_50_close)
+        .property("g0", &Parameters::g0)
         .property("vpd_dryness_factor", &Parameters::vpd_dryness_factor)
         .property("theta_moisture_factor", &Parameters::theta_moisture_factor)
         // Add any other properties you want to adjust from the web UI here
